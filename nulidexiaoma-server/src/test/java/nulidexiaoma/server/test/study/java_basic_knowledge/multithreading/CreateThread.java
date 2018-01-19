9ba51ae63849731ff534fce124da49da8523f9b7
wenzailong <994108345@qq.com>
2018-01-19 18:00:00 +0800

    日常提交(角色管理)

---
+++ nulidexiaoma-server/src/test/java/nulidexiaoma/server/test/study/java_basic_knowledge/multithreading/CreateThread.java
@@ -0,0 +1,206 @@
+package nulidexiaoma.server.test.study.java_basic_knowledge.multithreading;
+
+import nulidexiaoma.server.test.LoginServiceTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by wenzailong on 2018/1/19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring-config.xml")
+public class CreateThread {
+    private Logger logger = LoggerFactory.getLogger(LoginServiceTest.class);
+    /*
+    创建多线程主要分为两种方式，第一种为继承Thread类。第二种为实现Runnable接口
+     */
+
+    /*调用“继承Thread类创建线程”*/
+    @Test
+    public void createByThredTest(){
+        CreateByThred thread1 = new CreateByThred("A");
+        CreateByThred thread2 = new CreateByThred("B");
+        thread1.start();//调用此方法时，会自动调用线程类的run()方法。
+        thread2.start();
+    }
+    /**
+     * 继承Thread类创建线程
+     */
+    class CreateByThred extends Thread{
+        String name;
+        public CreateByThred(String name){
+            this.name = name;
+        }
+
+        /*线程会交替执行该方法*/
+        public void run(){
+            for(int i = 0;i<100;i++){
+                System.out.println(name+":"+i);
+            }
+        }
+    }
+
+    /*调用“实现Runnable接口”*/
+    @Test
+    public void createByRunnableTest(){
+        CreateThreadByRunnable thread1 = new CreateThreadByRunnable("c");
+        CreateThreadByRunnable thread2 = new CreateThreadByRunnable("d");
+        Thread threads1= new Thread(thread1);
+        Thread threads2= new Thread(thread2);
+        threads1.start();//调用此方法时，会自动调用线程类的run()方法。
+        threads2.start();
+
+    }
+    /**
+     * 实现Runnable接口
+     */
+    class CreateThreadByRunnable implements Runnable{
+        String name;
+        public CreateThreadByRunnable(String name){
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0;i<100;i++){
+                System.out.println(name+":"+i);
+            }
+        }
+    }
+
+
+    /**
+     * 测试join方法
+     */
+    class ThredJoin implements  Runnable{
+        String name;
+        public ThredJoin(String name){
+            this.name = name;
+        }
+        @Override
+        public void run() {
+            System.out.println(name+":子线程开始了");
+            for(int i = 0;i<20;i++){
+                System.out.println(name+":"+i);
+            }
+            System.out.println(name+":子线程结束了");
+        }
+    }
+
+    /**
+     * 测试josin方法
+     * @throws InterruptedException
+     */
+    @Test
+    public void threadJoinTest() throws InterruptedException {
+        System.out.println("主线程开始了");
+        ThredJoin threadJoin1 = new ThredJoin("A");
+        ThredJoin threadJoin2 = new ThredJoin("B");
+        Thread thread1 = new Thread(threadJoin1);
+        Thread thread2 = new Thread(threadJoin2);
+        thread1.start();
+        thread2.start();
+        thread1.join();//当thread线程运行结束的时候，才会执行以下代码！
+        System.out.println("主线程结束了");
+
+
+    }
+
+    /**
+     * 测试yield()
+     */
+    class yieldThread implements Runnable{
+
+        String name;
+        public yieldThread(String name){
+            this.name = name;
+        }
+        @Override
+        public void run() {
+            System.out.println(name+":子线程开始了");
+            for(int i = 0;i<20;i++){
+                System.out.println(name+":"+i);
+            }
+            System.out.println(name+":子线程结束了");
+        }
+    }
+    /**
+     * 测试josin方法
+     * @throws InterruptedException
+     */
+    @Test
+    public void yieldThreadTest() throws InterruptedException {
+        System.out.println("主线程开始了");
+        yieldThread threadJoin1 = new yieldThread("A");
+        yieldThread threadJoin2 = new yieldThread("B");
+        Thread thread1 = new Thread(threadJoin1);
+        Thread thread2 = new Thread(threadJoin2);
+        thread1.start();
+        thread2.start();
+        thread1.yield();//使该进程进入可运行状态，它把运行机会让给了同等优先级的其他线程，但是具体谁活的cpu还是随机的！
+        System.out.println("停止A线程");
+        System.out.println("主线程结束了");
+
+
+    }
+
+
+    /**
+     * 测试锁对象
+     */
+    class SynchronizedTest implements Runnable{
+        private String name;
+        private Object prev;
+        private Object self;
+        private SynchronizedTest(String name,Object prev,Object self){
+            this.name = name;
+            this.prev = prev;
+            this.self = self;
+        }
+
+        @Override
+        public void run() {
+            int count = 10;
+            while(count >0) {
+
+                synchronized (prev) {
+
+                    synchronized (self) {
+                        System.out.print(name);
+                        count--;
+                        self.notify();
+                    }
+                    try {
+                        prev.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    @Test
+    public void synchronizedTest(){
+        Object a = new Object();
+        Object b = new Object();
+        Object c = new Object();
+
+        SynchronizedTest t1 = new SynchronizedTest("A",c,a);
+        SynchronizedTest t2 = new SynchronizedTest("B",a,b);
+        SynchronizedTest t3 = new SynchronizedTest("C",b,c);
+        Thread thread1 = new Thread(t1);
+        Thread thread2 = new Thread(t2);
+        Thread thread3 = new Thread(t3);
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+}
+
+
+
